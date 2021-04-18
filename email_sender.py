078260ff65265bec3c1fe721c8152e03b0dd96a9
Vylmion <thomas.palenius@protonmail.com>
2021-04-18 19:07:51 +0300

    Email Sender

---
+++ email_sender.py
@@ -0,0 +1,20 @@
+import smtplib
+from email.message import EmailMessage
+from string import Template
+from pathlib import Path
+
+hmtl = Template(Path("index.html").read_text())
+email = EmailMessage()
+email["from"] = "xxx"  # Add sender name
+email["to"] = "email"  # Add here the email address you want to send TO
+email["subject"] = "This is a test"
+
+email.set_content(hmtl.substitute({"name": "TinTin"}), "html")
+
+with smtplib.SMTP(host="smtp.gmail.com", port=587) as smtp:
+    smtp.ehlo()
+    smtp.starttls()
+    # Add bellow the emails address where you send FROM and password.
+    smtp.login("email", "password")
+    smtp.send_message(email)
+    print("Jobs Done!")
